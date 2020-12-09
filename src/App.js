ef7b07191c436feebe922047bffc051eafddd094
Dheeraj Malik <37261043+dheerajmalik1@users.noreply.github.com>
2020-12-09 16:57:20 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+const emojiDictionary = {
+  "": " ",
+  " ": " ",
+  "😗": ":-*",
+  "😉": "Wink",
+  ":)": "Smilling",
+  ":-)": "Smilling",
+  ":(": "Sad",
+  ":-(": "Sad",
+  ":P": "Naughty",
+  ":p": "Naughty",
+  ":D": "Hehe",
+  "<3": "Love"
+};
+
+let emojisWeKnow = Object.keys(emojiDictionary);
+
+export default function App() {
+  const [meaning, setEmojiMeaning] = useState("");
+
+  function emojiInputHandler(event) {
+    const emojiInput = event.target.value;
+    let meaning = emojiDictionary[emojiInput];
+
+    if (meaning === undefined) {
+      meaning = "We dont have this emoji in our DB :(";
+    }
+    setEmojiMeaning(meaning);
+  }
+
+  return (
+    <div style={{}} className="App">
+      <h1>Welcome to emoji interpretor</h1>
+      <h4>Enter an emoji like :) :-) etc and know their meaning</h4>
+      <input onChange={emojiInputHandler} />
+      <p style={{ backgroundColor: "yellow" }}>{meaning}</p>
+      <h3>Emojis we know </h3>
+      {emojisWeKnow.map((emojis) => {
+        return (
+          <span key={emojis} style={{ padding: ".7rem", fontSize: "2rem" }}>
+            {emojis}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
